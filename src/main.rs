ca61fae8537c48147107a64981f6216fe9e9d5b5
mc_fdc <masato190411@icloud.com>
2023-11-14 11:12:49 +0000

    いろいろ～

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,36 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-#[tokio::main]
+use dotenvy::dotenv;
+use sqlx::mysql::MySqlPool;
+use std::env;
+
+struct AppState {
+    pool: MySqlPool,
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/callback")]
+async fn callback() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL")?;
+    let pool = MySqlPool::connect(&database_url).await?;
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+            .service(callback)
+            .app_data(AppState { pool: pool.clone() })
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await?;
     Ok(())
 }
